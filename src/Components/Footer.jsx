7ffd216f4009bd31787969908d9a602efa4334a3
Saifulkhandaker <khsaiful67@gmail.com>
2024-01-14 20:22:41 +0600

    link added with icon in footer section

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,4 @@
-
+import { FaFacebookF, FaInstagram, FaTwitter   } from "react-icons/fa";
 
 const Footer = () => {
     return (
@@ -14,6 +14,11 @@ const Footer = () => {
                 <div className="bg-[#111827] flex-1 py-16 lg:pl-28 text-center lg:text-left">
                   <h1 className="text-2xl pb-5">FOLLOW US</h1>
                   <p>Join Us on Social Media</p>
+                  <div className="flex justify-center lg:justify-start pt-5 gap-5 text-2xl items-center text-center">
+                    <a href="https://www.facebook.com/" target="_blank"><FaFacebookF /></a>
+                    <a href="https://www.instagram.com/" target="_blank"><FaInstagram /></a>
+                    <a href="https://www.twitter.com/" target="_blank"><FaTwitter /></a>
+                  </div>
                 </div>
             </div>
             <div className="text-white text-xs lg:text-sm text-center bg-[#151515] py-2">
